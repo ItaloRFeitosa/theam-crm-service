f9d0db2b09973860650b4365d6b8bb499c786daf
Italo Feitosa <italo85199@gmail.com>
2022-05-17 14:56:15 -0300

    feat(application/config): add main error middleware

--- src/application/config/error.js
+++ src/application/config/error.js
@@ -1,5 +1,19 @@
+const isJSONBadlyFormattedError = (err) => {
+  return err.name == "SyntaxError" && err.message.includes("JSON")
+}
+
+
 function setupErrorMiddleware(app, logger = console) {
   app.use(function (err, req, res, next) {
+    if(isJSONBadlyFormattedError(err)){
+      return res.status(400).json({
+        error:{
+          name: "BadlyFormattedJSON",
+          reason: [err.message],
+        },
+      });
+    }
+
     logger.error(`[InternalError][${req.traceId}]:`, err)
 
     res.status(500).json({
