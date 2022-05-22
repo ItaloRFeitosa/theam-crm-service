0c6cb442fc575df93c84c065660c0e24eff7e205
Italo Feitosa <italo85199@gmail.com>
2022-05-22 19:42:32 -0300

    feat(modules/customer): changes to module

--- src/modules/customer/customer-photo-provider.js
+++ src/modules/customer/customer-photo-provider.js
@@ -1,3 +1,44 @@
-const CustomerPhotoProvider = () => ({ createPhoto: () => console.log("not-implemented")})
+const { CUSTOMERS_BUCKET_NAME } = require("#application/config/constants");
+const { putSignedUrl, getSignedUrl } = require("#external/aws/s3");
 
-module.exports = { CustomerPhotoProvider }
+const CustomerPhotoProviderName = {
+  AWS: "aws",
+};
+
+const CustomerPhotoData = (props) => ({
+  provider: CustomerPhotoProviderName.AWS,
+  bucket: props.bucket || CUSTOMERS_BUCKET_NAME,
+  key: `${props.customerId}/photo.${props.extension}`,
+});
+
+const CustomerPhotoProvider = () => ({
+  putPhoto: async (props) => {
+    const contentType = `image/*`;
+    const photo = CustomerPhotoData(props);
+    const signedUrl = await putSignedUrl({
+      bucket: photo.bucket,
+      key: photo.key,
+      contentType,
+    });
+    return {
+      ...photo,
+      signedUrl,
+    };
+  },
+
+  signPhoto: async ({ photo, ...customer }) => {
+    if(!photo){
+      return {
+        ...customer,
+        photoUrl: null,
+      }
+    }
+    const signedUrl = await getSignedUrl(photo);
+    return {
+      ...customer,
+      photoUrl: signedUrl,
+    };
+  },
+});
+
+module.exports = { CustomerPhotoProvider };
