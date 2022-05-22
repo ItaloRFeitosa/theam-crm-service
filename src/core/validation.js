7d55e27795fd91dc15b56e1407be0bf44bdfe0b1
Italo Feitosa <italo85199@gmail.com>
2022-05-22 19:40:28 -0300

    feat(external/core/validation): add id schema validation

--- src/core/validation.js
+++ src/core/validation.js
@@ -1,5 +1,6 @@
+const { ObjectId } = require("mongoose").Types;
 const schema = require("yup");
-
+ObjectId.isValid;
 const { errorOf, isError } = require("./error");
 const { Result } = require("./result");
 
@@ -14,7 +15,17 @@ const Validation = (validator) => async (input) => {
     return Result.fail(ValidationError.error(maybeError.errors));
   }
 
-  return Result.ok()
+  return Result.ok();
 };
 
-module.exports = { ValidationError, schema, Validation };
+const isValidId = (idCheckers) => (value) => idCheckers.some((fn) => fn(value));
+
+const IdSchema = schema
+  .string()
+  .test(
+    "should be a valid id",
+    "${path} must be a valid id",
+    isValidId([ObjectId.isValid])
+  );
+
+module.exports = { ValidationError, schema, Validation, IdSchema };
