76b70960dfe16157acd37b3bbd54a044c570f71d
Italo Feitosa <italo85199@gmail.com>
2022-05-22 19:48:32 -0300

    feat(application/config/express-adpter): add middleware adapter

--- src/application/config/express-adapter.js
+++ src/application/config/express-adapter.js
@@ -1,24 +1,22 @@
 const { Request } = require("#core/http/request");
 
-const adaptRequest = (req) => Request({
-  traceId: req.traceId,
-  body: req.body,
-  headers: req.headers,
-  query: req.query,
-  params: req.params,
-  // user: req.user,
-  user: {
-    id: "fake-id"
-  },
-})
+const adaptRequest = (req) =>
+  Request({
+    traceId: req.traceId,
+    body: req.body,
+    headers: req.headers,
+    query: req.query,
+    params: req.params,
+    user: req.user,
+    path: req.baseUrl
+  });
 
-const adaptController = (controllerMethod) => async (req, res, next) => {
+const adaptController = (controllerMethod, hateoas = (_, res) => res) => async (req, res, next) => {
   try {
-    const request = adaptRequest(req)
-
+    const request = adaptRequest(req);
     const response = await controllerMethod(request);
-
-    return res.status(response.statusCode).json(response.data);
+    const hateoasResponse = hateoas(request, response)
+    return res.status(hateoasResponse.statusCode).json(hateoasResponse.body);
   } catch (error) {
     next(error);
   }
@@ -26,18 +24,28 @@ const adaptController = (controllerMethod) => async (req, res, next) => {
 
 const adaptValidation = (validation) => async (req, res, next) => {
   try {
-    const request = adaptRequest(req)
-
+    const request = adaptRequest(req);
     const result = await validation(request);
-
-    if(result.isError){
-      return res.status(400).json(result.error)
-    }
-
-    return next()
+    if (result.isError) return res.status(400).json({ error: result.error });
+    return next();
   } catch (error) {
     next(error);
   }
 };
 
-module.exports = { adaptController, adaptValidation };
+const adaptMiddleware = (middlewareMethod) => async (req, res, next) => {
+  try {
+    const request = adaptRequest(req);
+    const middlewareResponse = await middlewareMethod(request);
+    if (!middlewareResponse.next) {
+      return res.status(middlewareResponse.statusCode).json(middlewareResponse.body);
+    }
+    // merge response data with express request
+    Object.assign(req, middlewareResponse.body);
+    return next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { adaptController, adaptValidation, adaptMiddleware };
